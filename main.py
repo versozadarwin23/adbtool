af1192b8224dfb155e2a33d8d8f8d9b2becdaba1
versozadarwin23 <109713017+versozadarwin23@users.noreply.github.com>
2025-10-14 03:27:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import shutil
 import tempfile
 
 # --- App Version and Update URL ---
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 UPDATE_URL = "https://raw.githubusercontent.com/versozadarwin23/adbtool/refs/heads/main/main.py"
 VERSION_CHECK_URL = "https://raw.githubusercontent.com/versozadarwin23/adbtool/refs/heads/main/version.txt"
 
@@ -1871,6 +1871,7 @@ if __name__ == '__main__':
     app = AdbControllerApp()
     app.mainloop()
 #ok
+
 
 
 
